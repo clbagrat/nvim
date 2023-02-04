49e87858d7753aa49ee8a390404f30fc720bf8aa
bagrat <gobedashvilibagrat@gmail.com>
2023-02-04 22:51:31 +0100

    trying telescope

--- lua/bgi/keybinds.lua
+++ lua/bgi/keybinds.lua
@@ -27,3 +27,11 @@ map('n', '<leader>p', '"*p')
 map('v', '<leader>p', '"*p')
 map('n', '<leader>y', '"*y')
 map('v', '<leader>y', '"*y')
+
+
+  
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- lua/bgi/plugins.lua
+++ lua/bgi/plugins.lua
@@ -50,6 +50,11 @@ return require('packer').startup({
 
         use 'mhartington/formatter.nvim'
 
+        use {
+          'nvim-telescope/telescope.nvim', tag = '0.1.1',
+          requires = { {'nvim-lua/plenary.nvim'} }
+        }
+
         if packer_bootstrap then
             require('packer').sync()
         end
